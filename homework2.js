e63a13fc5890f97494336989b39e59dddda79831
Cinquep <cinquep22@gmail.com>
2026-03-24 17:28:17 -0500

    Update homework2.js

--- homework2.js
+++ homework2.js
@@ -64,3 +64,137 @@ function getdata1()
       document.getElementById("outputformdata").innerHTML = formoutput;
    }
 }
+ 
+ function checkfirstname()
+    {
+        x = document.getElementById("firstname").value;
+        if( x.length<2) { 
+              document.getElementById("name_message").innerHTML = "Invalid name... too short.";  
+              error_flag = 1;
+        }
+        else {
+            if (x.match(/[a-zA-Z3-5'-]+$/)) {
+              document.getElementById("name_message").innerHTML = "";  
+            }
+            else  {
+              document.getElementById("name_message").innerHTML = "Invalid characters in name.";
+              error_flag = 1;
+              }
+        }
+    }
+ 
+function checkmiddle()
+    {
+        x = document.getElementById("middleinit").value;
+        if( x.length>0) { 
+              if (x.match(/[a-zA-Z ]/)) {
+              document.getElementById("name_message").innerHTML = "";  
+            }
+            else  {
+              document.getElementById("name_message").innerHTML = "Invalid characters in name.";
+              error_flag = 1;
+              }
+        }
+    }
+ 
+function checklastname()
+    {
+        x = document.getElementById("lastname").value;
+        if( x.length<2) { 
+              document.getElementById("name_message").innerHTML = "Invalid name... too short.";
+              error_flag = 1;  
+        }
+        else {
+            if (x.match(/[a-zA-Z3-5'-]+$/)) {
+              document.getElementById("name_message").innerHTML = "";  
+            }
+            else  {
+              document.getElementById("name_message").innerHTML = "Invalid characters in name.";
+              error_flag = 1;
+              }
+        }
+    }
+ 
+ function passwordentry() 
+    {
+    var passwordoutput;
+    var passwordinput = document.getElementById("password1").value;
+    console.log(passwordinput);
+    // Validate lowercase letters
+    if(passwordinput.search(/[a-z]/) < 0 ) {
+      passwordoutput = "Enter At least 1 lower case letter";
+      error_flag = 1;
+    } else {
+      passwordoutput = "";
+    }
+
+ function checkpassword2() {
+    x=document.getElementById("password1").value;
+    y=document.getElementById("password2").value;
+    if ( x==y ) 
+    {
+      document.getElementById("password2_text").innerHTML = "Passwords match!";
+    } else  
+      {
+         document.getElementById("password2_text").innerHTML = "Passwords DO NOT match!";
+         error_flag = 1;
+      }
+    }
+
+ function checkaddr1() {
+    x = document.getElementById("addr1").value;
+    console.log(x.value);
+    console.log(x.length);
+    if (x.length < 2 ) {  
+      document.getElementById("addr1_message").innerHTML = "Enter something on address line";  
+      error_flag = 1; 
+      console.log(error_flag);
+      }
+      else { 
+          document.getElementById("addr1_message").innerHTML = "";  
+      }
+      console.log(addr1_message);
+}
+function checkaddr2() {}
+
+function checkcity() {
+         if (document.getElementById("city").value.match(/^[ a-zA-Z3-5'-]+$/)) {
+              document.getElementById("city_message").innerHTML = "";  
+            }
+            else  {
+              document.getElementById("city_message").innerHTML = "Invalid characters in City name.";
+              error_flag = 1;
+              }
+}
+function checkstate() {
+        z=document.getElementById("state").value;
+        if(z=="") { 
+              document.getElementById("state_message").innerHTML = "Please choose a state";  
+              error_flag = 1;
+        }
+        else {
+          document.getElementById("state_message").innerHTML = ""; 
+        }
+}
+//    if (document.getElementById("state").length = 0 ) {  error_flag = 1; }
+
+// Check everything
+    function checkform() 
+      {
+        error_flag = "0";
+        checkfirstname();
+        checkmiddle();
+        checklastname();
+        checkaddr1();
+        checkaddr2();
+        // etc...
+        passwordentry();
+        console.log("Error flag: "+error_flag);
+        if (error_flag == "1")
+        {
+          alert("Please fix the indicated errors!");
+        }
+        else {
+          document.getElementById("submit").disabled = false;
+        }
+      }
