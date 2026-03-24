388fa7a9412ab47a5adb749d378545980c673e2b
Cinquep <cinquep22@gmail.com>
2026-03-24 16:43:20 -0500

    Update homework2.js

--- homework2.js
+++ homework2.js
@@ -5,3 +5,62 @@
  Date Updated: 2026-03-24
  Purpose: Redisplay/validate data from a form
 */
+function setup()
+  {
+    let firstnameflag;
+    let middleflag;
+    let lastnameflag;
+    let addr1flag;
+    let password1flag;
+    let password2flag;
+    /* lastnameflag addr1flag password1flag password2flag;  */
+("setting up for the first run...");
+    checkCookie();
+    getdata1();
+    
+  }
+function getdata1() 
+{
+  var formcontents = document.getElementById("signup");
+  var formoutput;
+  var datatype;
+  var i;
+  formoutput = "<table class='output'><th>Dataname</th><th>Type</th><th>Value</th>";
+  for (i = 0; i < formcontents.length; i++) {
+            console.log("item: "+i+" "+formcontents.elements[i].name+" = "+formcontents.elements[i].value);
+            if (formcontents.elements[i].value !="") {
+              datatype = formcontents.elements[i].type;
+              switch (datatype) {
+                case "checkbox":
+                  if (formcontents.elements[i].checked) {
+                    formoutput = formoutput + "<tr><td align='right'>"+formcontents.elements[i].name+"</td>";
+                    formoutput = formoutput +"<td align='right'>"+ datatype + "</td>";
+                    formoutput = formoutput +"<td class='outputdata'>Checked</td></tr>";
+                  }
+                  break;
+               case "radio":
+                    if (formcontents.elements[i].checked) {
+                      formoutput = formoutput + "<tr><td align='right'>"+formcontents.elements[i].name+"</td>";
+                      formoutput = formoutput +"<td align='right'>"+ datatype + "</td>";
+                      formoutput = formoutput +"<td class='outputdata'>"+ formcontents.elements[i].value+"</td></tr>";
+                    }
+                  break;
+                case "button": case "submit": case "reset":
+                  formoutput = formoutput + "<tr><td align='right'>"+formcontents.elements[i].name+"</td>";
+                  formoutput = formoutput +"<td align='right'>"+ datatype + "</td>";
+                  formoutput = formoutput +"<td class='outputdata'>"+ formcontents.elements[i].value+"</td></tr>";
+                  break;
+                default:
+                  formoutput = formoutput + "<tr><td align='right'>"+formcontents.elements[i].name+"</td>";
+                  formoutput = formoutput +"<td align='right'>"+ datatype + "</td>";
+                  formoutput = formoutput +"<td class='outputdata'>"+ formcontents.elements[i].value+"</td></tr>";
+                }
+        
+
+  }
+
+   if (formoutput.length>0) { 
+      formoutput = formoutput + "</table>";
+      document.getElementById("outputformdata").innerHTML = formoutput;
+   }
+}
