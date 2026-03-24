29cc7c46fb3b166a11470a53ce5eee440a34f5dd
Cinquep <cinquep22@gmail.com>
2026-03-24 17:37:58 -0500

    Update homework2.js

--- homework2.js
+++ homework2.js
@@ -5,196 +5,189 @@
  Date Updated: 2026-03-24
  Purpose: Redisplay/validate data from a form
 */
-function setup()
-  {
-    let firstnameflag;
-    let middleflag;
-    let lastnameflag;
-    let addr1flag;
-    let password1flag;
-    let password2flag;
-    /* lastnameflag addr1flag password1flag password2flag;  */
-("setting up for the first run...");
-    checkCookie();
-    getdata1();
-    
-  }
-function getdata1() 
-{
-  var formcontents = document.getElementById("register");
+
+function setup() {
+  checkCookie();
+  getdata1();
+}
+
+function getdata1() {
+  var formcontents = document.getElementById("register"); 
   var formoutput;
   var datatype;
   var i;
-  formoutput = "<table class='output'><th>Dataname</th><th>Type</th><th>Value</th>";
-  for (i = 0; i < formcontents.length; i++) {
-            console.log("item: "+i+" "+formcontents.elements[i].name+" = "+formcontents.elements[i].value);
-            if (formcontents.elements[i].value !="") {
-              datatype = formcontents.elements[i].type;
-              switch (datatype) {
-                case "checkbox":
-                  if (formcontents.elements[i].checked) {
-                    formoutput = formoutput + "<tr><td align='right'>"+formcontents.elements[i].name+"</td>";
-                    formoutput = formoutput +"<td align='right'>"+ datatype + "</td>";
-                    formoutput = formoutput +"<td class='outputdata'>Checked</td></tr>";
-                  }
-                  break;
-               case "radio":
-                    if (formcontents.elements[i].checked) {
-                      formoutput = formoutput + "<tr><td align='right'>"+formcontents.elements[i].name+"</td>";
-                      formoutput = formoutput +"<td align='right'>"+ datatype + "</td>";
-                      formoutput = formoutput +"<td class='outputdata'>"+ formcontents.elements[i].value+"</td></tr>";
-                    }
-                  break;
-                case "button": case "submit": case "reset":
-                  formoutput = formoutput + "<tr><td align='right'>"+formcontents.elements[i].name+"</td>";
-                  formoutput = formoutput +"<td align='right'>"+ datatype + "</td>";
-                  formoutput = formoutput +"<td class='outputdata'>"+ formcontents.elements[i].value+"</td></tr>";
-                  break;
-                default:
-                  formoutput = formoutput + "<tr><td align='right'>"+formcontents.elements[i].name+"</td>";
-                  formoutput = formoutput +"<td align='right'>"+ datatype + "</td>";
-                  formoutput = formoutput +"<td class='outputdata'>"+ formcontents.elements[i].value+"</td></tr>";
-                }
-        
 
+  formoutput = "<table class='output'><tr><th>Field Name</th><th>Type</th><th>Value</th></tr>";
+
+  for (i = 0; i < formcontents.elements.length; i++) {
+    var el = formcontents.elements[i];
+    datatype = el.type;
+
+    switch (datatype) {
+      case "checkbox":
+        if (el.checked) {
+          formoutput += "<tr><td align='right'>" + el.name + "</td>";
+          formoutput += "<td align='right'>" + datatype + "</td>";
+          formoutput += "<td class='outputdata'>Checked</td></tr>";
+        }
+        break;
+      case "radio":
+        if (el.checked) {
+          formoutput += "<tr><td align='right'>" + el.name + "</td>";
+          formoutput += "<td align='right'>" + datatype + "</td>";
+          formoutput += "<td class='outputdata'>" + el.value + "</td></tr>";
+        }
+        break;
+      case "button": case "submit": case "reset":
+        // skip buttons
+        break;
+      case "password":
+        if (el.value != "") {
+          formoutput += "<tr><td align='right'>" + el.name + "</td>";
+          formoutput += "<td align='right'>" + datatype + "</td>";
+          formoutput += "<td class='outputdata'>••••••••</td></tr>";
+        }
+        break;
+      default:
+        if (el.value != "") {
+          formoutput += "<tr><td align='right'>" + el.name + "</td>";
+          formoutput += "<td align='right'>" + datatype + "</td>";
+          formoutput += "<td class='outputdata'>" + el.value + "</td></tr>";
+        }
+    }
   }
 
-   if (formoutput.length>0) { 
-      formoutput = formoutput + "</table>";
-      document.getElementById("outputformdata").innerHTML = formoutput;
-   }
+  formoutput += "</table>";
+  document.getElementById("outputformdata").innerHTML = formoutput;
 }
- 
- function checkfirstname()
-    {
-        x = document.getElementById("firstname").value;
-        if( x.length<2) { 
-              document.getElementById("name_message").innerHTML = "Invalid name... too short.";  
-              error_flag = 1;
-        }
-        else {
-            if (x.match(/[a-zA-Z3-5'-]+$/)) {
-              document.getElementById("name_message").innerHTML = "";  
-            }
-            else  {
-              document.getElementById("name_message").innerHTML = "Invalid characters in name.";
-              error_flag = 1;
-              }
-        }
-    }
- 
-function checkmiddle()
-    {
-        x = document.getElementById("middleinit").value;
-        if( x.length>0) { 
-              if (x.match(/[a-zA-Z ]/)) {
-              document.getElementById("name_message").innerHTML = "";  
-            }
-            else  {
-              document.getElementById("name_message").innerHTML = "Invalid characters in name.";
-              error_flag = 1;
-              }
-        }
-    }
- 
-function checklastname()
-    {
-        x = document.getElementById("lastname").value;
-        if( x.length<2) { 
-              document.getElementById("name_message").innerHTML = "Invalid name... too short.";
-              error_flag = 1;  
-        }
-        else {
-            if (x.match(/[a-zA-Z3-5'-]+$/)) {
-              document.getElementById("name_message").innerHTML = "";  
-            }
-            else  {
-              document.getElementById("name_message").innerHTML = "Invalid characters in name.";
-              error_flag = 1;
-              }
-        }
-    }
- 
- function passwordentry() 
-    {
-    var passwordoutput;
-    var passwordinput = document.getElementById("password1").value;
-    console.log(passwordinput);
-    // Validate lowercase letters
-    if(passwordinput.search(/[a-z]/) < 0 ) {
-      passwordoutput = "Enter At least 1 lower case letter";
-      error_flag = 1;
+
+function removedata1() {
+  document.getElementById("outputformdata").innerHTML = "";
+}
+
+function checkfirstname() {
+  var x = document.getElementById("firstname").value;
+  if (x.length < 2) {
+    document.getElementById("name_message").innerHTML = "Invalid name... too short.";
+    error_flag = 1;
+  } else {
+    if (x.match(/^[a-zA-Z'-]+$/)) {
+      document.getElementById("name_message").innerHTML = "";
     } else {
-      passwordoutput = "";
+      document.getElementById("name_message").innerHTML = "Invalid characters in name.";
+      error_flag = 1;
     }
-
- function checkpassword2() {
-    x=document.getElementById("password1").value;
-    y=document.getElementById("password2").value;
-    if ( x==y ) 
-    {
-      document.getElementById("password2_text").innerHTML = "Passwords match!";
-    } else  
-      {
-         document.getElementById("password2_text").innerHTML = "Passwords DO NOT match!";
-         error_flag = 1;
-      }
-    }
-
- function checkaddr1() {
-    x = document.getElementById("addr1").value;
-    console.log(x.value);
-    console.log(x.length);
-    if (x.length < 2 ) {  
-      document.getElementById("addr1_message").innerHTML = "Enter something on address line";  
-      error_flag = 1; 
-      console.log(error_flag);
-      }
-      else { 
-          document.getElementById("addr1_message").innerHTML = "";  
-      }
-      console.log(addr1_message);
+  }
 }
+
+function checkmiddle() {
+  var x = document.getElementById("middleinit").value;
+  if (x.length > 0) {
+    if (x.match(/[a-zA-Z]/)) {
+      document.getElementById("name_message").innerHTML = "";
+    } else {
+      document.getElementById("name_message").innerHTML = "Invalid characters in middle initial.";
+      error_flag = 1;
+    }
+  }
+}
+
+function checklastname() {
+  var x = document.getElementById("lastname").value;
+  if (x.length < 2) {
+    document.getElementById("name_message").innerHTML = "Invalid name... too short.";
+    error_flag = 1;
+  } else {
+    if (x.match(/^[a-zA-Z'-]+$/)) {
+      document.getElementById("name_message").innerHTML = "";
+    } else {
+      document.getElementById("name_message").innerHTML = "Invalid characters in name.";
+      error_flag = 1;
+    }
+  }
+}
+
+function passwordentry() {
+  var passwordoutput = "";
+  var passwordinput = document.getElementById("pass").value; 
+
+  if (passwordinput.search(/[a-z]/) < 0) {
+    passwordoutput += "Need at least 1 lowercase letter. ";
+    error_flag = 1;
+  }
+  if (passwordinput.search(/[A-Z]/) < 0) {
+    passwordoutput += "Need at least 1 uppercase letter. ";
+    error_flag = 1;
+  }
+  if (passwordinput.search(/[0-9]/) < 0) {
+    passwordoutput += "Need at least 1 number. ";
+    error_flag = 1;
+  }
+
+  document.getElementById("pass_text").innerHTML = passwordoutput;
+}
+
+function checkpassword2() {
+  var x = document.getElementById("pass").value;        
+  var y = document.getElementById("confirmPass").value; 
+  if (x == y) {
+    document.getElementById("confirmPass_text").innerHTML = "Passwords match!";
+  } else {
+    document.getElementById("confirmPass_text").innerHTML = "Passwords DO NOT match!";
+    error_flag = 1;
+  }
+}
+
+function checkaddr1() {
+  var x = document.getElementById("addr1").value;
+  if (x.length < 2) {
+    document.getElementById("addr1_text").innerHTML = "Enter something on address line"; 
+    error_flag = 1;
+  } else {
+    document.getElementById("addr1_text").innerHTML = "";
+  }
+}
+
 function checkaddr2() {}
 
 function checkcity() {
-         if (document.getElementById("city").value.match(/^[ a-zA-Z3-5'-]+$/)) {
-              document.getElementById("city_message").innerHTML = "";  
-            }
-            else  {
-              document.getElementById("city_message").innerHTML = "Invalid characters in City name.";
-              error_flag = 1;
-              }
+  if (document.getElementById("city").value.match(/^[ a-zA-Z'-]+$/)) {
+    document.getElementById("City_text").innerHTML = "";
+  } else {
+    document.getElementById("City_text").innerHTML = "Invalid characters in City name.";
+    error_flag = 1;
+  }
 }
-function checkstate() {
-        z=document.getElementById("state").value;
-        if(z=="") { 
-              document.getElementById("state_message").innerHTML = "Please choose a state";  
-              error_flag = 1;
-        }
-        else {
-          document.getElementById("state_message").innerHTML = ""; 
-        }
-}
-//    if (document.getElementById("state").length = 0 ) {  error_flag = 1; }
 
-// Check everything
-    function checkform() 
-      {
-        error_flag = "0";
-        checkfirstname();
-        checkmiddle();
-        checklastname();
-        checkaddr1();
-        checkaddr2();
-        // etc...
-        passwordentry();
-        console.log("Error flag: "+error_flag);
-        if (error_flag == "1")
-        {
-          alert("Please fix the indicated errors!");
-        }
-        else {
-          document.getElementById("submit").disabled = false;
-        }
-      }
+function checkstate() {
+  var z = document.getElementById("state").value;
+  if (z == "") {
+    document.getElementById("State_text").innerHTML = "Please choose a state";
+    error_flag = 1;
+  } else {
+    document.getElementById("State_text").innerHTML = "";
+  }
+}
+
+function checkform() {
+  error_flag = "0";
+  checkfirstname();
+  checkmiddle();
+  checklastname();
+  checkaddr1();
+  checkaddr2();
+  checkcity();
+  checkstate();
+  passwordentry();
+  checkpassword2();
+
+  if (error_flag == "1") {
+    alert("Please fix the indicated errors!");
+  } else {
+    document.getElementById("submit").disabled = false;
+  }
+}
+
+function checkCookie() {
+  // placeholder
+}
